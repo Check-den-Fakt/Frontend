14f5a8503450fc43d66de521eb20bb5776544848
Samuel Mauch <samuel.mauch@linkit.tech>
2020-03-29 16:21:41 +0200

    fixed api call url orchestrator

--- src/constants/apisdk.js
+++ src/constants/apisdk.js
@@ -6,5 +6,5 @@ const apiMethodsNames = {
 
 export default {
     'REPORT': '/api/messagearchive',
-    'VALIDATE' :`/workflows/73896dc86d9149058d3bd5234fb4ae86/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=MmoB-9jJt1DbB25AfNJis889rx8b9LbJDX0zJSCsmoM`
+    'VALIDATE' :'https://prod-56.westeurope.logic.azure.com:443/workflows/73896dc86d9149058d3bd5234fb4ae86/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=MmoB-9jJt1DbB25AfNJis889rx8b9LbJDX0zJSCsmoM'
 };
