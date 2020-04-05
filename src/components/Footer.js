5858c12d846d334f7382ed335143dfc72018202f
Martina Freundorfer <martina.freundorfer@digitalcareerinstitute.org>
2020-04-05 15:58:23 +0200

    Team Map Bilder vergrößert

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,26 +6,26 @@ export default function Footer () {
     const { t } = useTranslation();
    
     return (
-        <footer className="footer m-5">
-            <div className="container">
+        <footer className="footer p-5">
                 <div className="row">
                     <div className="col fact-footer-col">
                         <a href="/imprint">
-                            {t('imprint')} &middot;
+                            {t('imprint')} 
                         </a>
                     </div>
+                    &middot;
                     <div className="col fact-footer-col">
                         <a href="/dsgvo">
-                            {t('dataPrivacyPolicy')} &middot;
+                            {t('dataPrivacyPolicy')} 
                         </a>
                     </div>
+                    &middot;
                     <div className="col fact-footer-col">
                         <a href="/imprint">
                             {t('Contact')}
                         </a>
                     </div>
                 </div>
-            </div>
         </footer>
     );
 }
